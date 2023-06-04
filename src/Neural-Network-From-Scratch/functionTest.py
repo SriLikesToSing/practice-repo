02ef76b86e63e5a12f600f9833ce14d85bb62ece
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2023-06-03 17:41:39 -0700

    neural network from scratch

---
+++ src/Neural-Network-From-Scratch/functionTest.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+#        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+
+
+data = np.random.randn(0, 1)
+print(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
